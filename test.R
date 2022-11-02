ed12dc0847e9568b0938ec47e06abd1808014ce2
Aimin Yan <aimin.at.work@gmail.com>
2022-11-02 17:41:56 -0400

    update readme

---
+++ test.R
@@ -0,0 +1,8 @@
+blat /home/aimin.yan/IS-Seq-python3/utilsRefData/IsSeq/hg38/hg38ChrOnly.fa /local_scratch/ISseqOutput/vcn/IsaByINSPIIREDTimeTest/fa/MOI30CLB6/R1_fastq_trim12nt_qcTrimmed_MatchBlastLtrLc_Barcode_FB-P5-Rd1-LTR.1.fq_trimwithCutAdapt_MOI30CLB6_ReadyToAlignSort.fa /local_scratch/ISseqOutput/vcn/IsaByINSPIIREDTimeTest/fa/MOI30CLB6/R1_fastq_trim12nt_qcTrimmed_MatchBlastLtrLc_Barcode_FB-P5-Rd1-LTR.1.fq_trimwithCutAdapt_MOI30CLB6_ReadyToAlignSort.fa.local.psl -tileSize=11 -stepSize=9 -minIdentity=85 -maxIntron=5 -minScore=27 -dots=1000 -out=psl -noHead
+
+
+https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_21/GRCh38.genome.fa.gz
+
+https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_38/GRCh38.primary_assembly.genome.fa.gz
+
+https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_40/GRCh38.primary_assembly.genome.fa.gz
