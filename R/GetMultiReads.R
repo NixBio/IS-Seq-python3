0dde57755ba38b360850b753f74b116968a05992
Aimin Yan <a.at.work@gmail.com>
2022-12-06 15:37:03 +0000

    add GetMultiReads.R

--- R/GetMultiReads.R
+++ R/GetMultiReads.R
@@ -1,14 +1,17 @@
-# Rscript $HOME/Aimin/IS-Seq-python3/R/GetMultiReads.R /Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE/Results.RData /Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE_multihitData/Results.RData ~/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0
+# Rscript $HOME/Aimin/IS-Seq-python3/R/GetMultiReads.R /local_scratch/ISseqOutput/vcn/IsaByINSPIIRED/fa/MOI30CLB7/IS0/multihitData.rds /local_scratch/ISseqOutput/vcn/IsaByINSPIIRED/fa/MOI30CLB7/IS0
+
+#/Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE/Results.RData /Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE_multihitData/Results.RData ~/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0
 
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)>=1) {
   # default output file
-  input_uniq_hit=args[1]
-  input_multi_hit=args[2]
-  output.dir=args[3]
+  #input_uniq_hit=args[1]
+  input_multi_hit=args[1]
+  output.dir=args[2]
 }
 
-multihit <- readRDS('/Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/MOI30CLB7/IS0/multihitData.rds')
+multihit <- readRDS(input_multi_hit)
+saveRDS(multihit$unclusteredMultihits,file.path(output.dir,"multihit_allSites.rds"))
 
-saveRDS(multihit$unclusteredMultihits,'/Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/MOI30CLB7/IS0/multihit_allSites.rds')
+
---
+++ R/GetMultiReadsScript.R
@@ -0,0 +1,33 @@
+# Rscript $HOME/Aimin/IS-Seq-python3/R/GetMultiReadsScript.R /local_scratch/ISseqOutput/vcn/IsaByINSPIIRED/fa
+
+initial.options <- commandArgs(trailingOnly = FALSE)
+print(initial.options)
+
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.dirname <- dirname(script.name)
+print(script.dirname)
+
+packages <- c("optparse","stringr")
+
+zzz<-lapply(packages, function(xxx) suppressMessages(library(xxx, character.only = TRUE,quietly=TRUE,warn.conflicts = FALSE)))
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+} else if (length(args)>=1) {
+  # default output file
+  #input_uniq_hit=args[1]
+  input.dir=args[1]
+}
+
+input.files <- list.files(input.dir,pattern = "multihitData.rds",recursive = TRUE,full.names=TRUE)
+
+null <- lapply(input.files, function(u){
+  cmd=paste0("Rscript ",file.path(script.dirname,"GetMultiReads.R ") ,u,' ',dirname(u))
+  cat(cmd,'\n')
+  system(cmd)
+})
+
+
