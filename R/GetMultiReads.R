f56a4f45fb6c81e247da6246bd34587a09920c4d
Aimin Yan <a.at.work@gmail.com>
2022-12-07 03:10:11 +0000

    change args

--- R/GetMultiReads.R
+++ R/GetMultiReads.R
@@ -2,16 +2,21 @@
 
 #/Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE/Results.RData /Users/c-aimin.yan/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0/FragMLE_multihitData/Results.RData ~/OneDrive/Aimin/Vcn_INSPIIRED/share/ISseqOutput/Dec282021/IsaByINSPIIREDTimeTest/fa/CL6/IS0
 
+
+args = commandArgs(trailingOnly=TRUE)
+
+#print(args)
+
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)>=1) {
-  # default output file
-  #input_uniq_hit=args[1]
-  input_multi_hit=args[1]
+  input.multi.hit=args[1]
   output.dir=args[2]
 }
 
-multihit <- readRDS(input_multi_hit)
+#print(input.multi.hit)
+
+multihit <- readRDS(input.multi.hit)
 saveRDS(multihit$unclusteredMultihits,file.path(output.dir,"multihit_allSites.rds"))
 
 
--- R/GetMultiReadsScript.R
+++ R/GetMultiReadsScript.R
@@ -14,6 +14,8 @@ zzz<-lapply(packages, function(xxx) suppressMessages(library(xxx, character.only
 
 args = commandArgs(trailingOnly=TRUE)
 
+#print(args[1])
+
 if (length(args)==0) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)>=1) {
